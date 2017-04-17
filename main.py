c5cf538771bb89fc8d6e8cdcabc83ee81f262fd5
Maki94 <markomihajlovicfm@gmail.com>
2017-04-17 16:44:26 +0200

    paper added

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from __future__ import division
 import collections
 import random
 import numpy as np
+import sys
 from nltk import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
-from sklearn import svm
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn import svm, cross_validation
+from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis, LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score
 from nltk.corpus import stopwords
 import codecs
 from nltk.stem.porter import PorterStemmer
-
+import os
+import warnings
+warnings.filterwarnings('ignore')
 
 def number_of_chars(s):
     return len(s)
@@ -81,13 +85,13 @@ def make_feature_vector(critique, labels):
 
 def read_data():
     ''' reads data files and returns lists of comments and labels'''
-    f = open('badCritiques.txt', 'r')
+    f = open(os.path.join(os.path.dirname(sys.argv[0]), r'./badCritiques.txt'), 'r')
     # f = open('bad.txt', 'r')
     bad = f.read().split('|')
     bad = [sentence.replace('\n', '') for sentence in bad]
     f.close()
 
-    f = open('goodCritiques.txt', 'r')
+    f = open(os.path.join(os.path.dirname(sys.argv[0]), r'.\goodCritiques.txt'), 'r')
     # f = open('good.txt', 'r')
     good = f.read().split('|')
     good = [sentence.replace('\n', '') for sentence in good]
@@ -110,10 +114,10 @@ def get_f1_score(Y_test, Y_predict):
     for i in range(test_size):
         if Y_predict[i] == Y_test[i]:
             score += 1
-    print 'Got %s out of %s' % (score, test_size)
-    print 'f1 macro = %.2f' % (f1_score(Y_test, Y_predict, average='macro'))
-    print 'f1 micro = %.2f' % (f1_score(Y_test, Y_predict, average='micro'))
-    print 'f1 weighted = %.2f' % (f1_score(Y_test, Y_predict, average='weighted'))
+    print('Got %s out of %s' % (score, test_size))
+    print('f1 macro = %.2f' % (f1_score(Y_test, Y_predict, average='macro')))
+    print('f1 micro = %.2f' % (f1_score(Y_test, Y_predict, average='micro')))
+    print('f1 weighted = %.2f' % (f1_score(Y_test, Y_predict, average='weighted')))
 
 
 def preprocessing(critiques, labels):
@@ -160,47 +164,30 @@ if __name__ == '__main__':
 
     critiques, labels = read_data()
     critiques, labels = preprocessing(critiques, labels)
-    # feature_vector = []
-
-
-    # X = np.array(dataset.data.drop([class_label], 1))
-    # y = np.array(dataset.data[class_label])
-    # X = preprocessing.scale(X)
-    # X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
-
-
 
     features_and_labels = make_feature_vector(critiques, labels)
     number_of_features = len(features_and_labels[0]) - 1
 
     # shuffle to mix good and bad ones
+    random.seed(0)
     random.shuffle(features_and_labels)
 
-    # make  train / test sets from the shuffled list
-    cut = int(len(features_and_labels) * 0.9)
-    XY_train = features_and_labels[:cut]
-    XY_test = features_and_labels[cut:]
+    xy = np.array(features_and_labels)
+    X = xy[:, 0:-1]
+    y = xy[:, -1]
 
-    X_train, Y_train = make_np_array_XY(XY_train)
-    X_test, Y_test = make_np_array_XY(XY_test)
-    print 'len(X_test) = %s len(Y_test) = %s' % (len(X_test), len(Y_test))
+    X_train, X_test, Y_train, Y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+    classifiers = {
+        'lda': LinearDiscriminantAnalysis().fit(X_train, Y_train),
+        'qda': QuadraticDiscriminantAnalysis().fit(X_train, Y_train),
+        'lr': LogisticRegression().fit(X_train, Y_train),
+        'svc': svm.SVC(kernel='linear').fit(X_train, Y_train),
+    }
+    # svc = LinearDiscriminantAnalysis().fit(X_train, Y_train)
 
-    # train set
-    C = 1.0  # SVM regularization parameter
-    # svc = svm.SVC(kernel='linear', C=C).fit(X_train, Y_train)
-    print "linear_model.SGDClassifier()..."
-    from sklearn import linear_model
-
-    # svc = linear_model.SGDClassifier().fit(X_train, Y_train)
-    svc = LinearDiscriminantAnalysis().fit(X_train, Y_train)
-
-    print "svc.predict()..."
-    Y_predict = svc.predict(X_test)
-
-    print 'Y_predict:\n', Y_predict
-    print 'Y_test:   \n', Y_test
-
-    # get f1 score
-    get_f1_score(Y_test, Y_predict)
-
-    # TODO cross_validation
+    for key, svc in classifiers.items():
+        Y_predict = svc.predict(X_test)
+        print(key, ':')
+        # print('Y_predict:\n', Y_predict)
+        # print('Y_test:   \n', Y_test)
+        get_f1_score(Y_test, Y_predict)
